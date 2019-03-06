eeee8a4dd0907d12fec22017198893d53d7f7420
persistLearning <1052905250@qq.com>
2019-03-06 16:25:25 +0800

    Add a new statement in Hello.java file.

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
     public static void main(String[] args){
         System.out.println("A test for upload project to github!");
+        System.out.println("Add a new statement");
     }
 }
